5a5123a77b36f4d8be9787e108b2e295b39fd880
abhi goyal <abhigoyal72621289@gmail.com>
2023-10-21 15:44:23 +0530

    first

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# RentoBuddy
+{
+    Project Report {
+        basicSetup: [nmp i int express ejs mongoose nodemon method-override ],
+        touch : app.js,
+        connDatabase : RentoBuddy,
+        createModule: Listing,
+    }
+
+
+}
---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const mongo_url = "mongodb://127.0.0.1:27017/RentoBuddy";
+
+const listing = require("../models/listing.js");
+
+main()
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+    console.error(err);
+})
+async function main(){
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async () =>{
+   await listing.deleteMany({});
+   await listing.insertMany(initData.data);
+   console.log("data was initialized");
+};
+
+initDB();
