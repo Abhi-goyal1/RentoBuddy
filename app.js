d32b32440880d21e99322f5260d3e3d20705bcba
abhi goyal <abhigoyal72621289@gmail.com>
2023-10-22 11:31:16 +0530

    sec

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
